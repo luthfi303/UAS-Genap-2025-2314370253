99b70967279b32b691cb5be283e3053726a07738
PRIANTI MARATUL LUTHFI <priantiluthfi@gmail.com>
2025-07-16 10:58:05 +0700

    Force HTTPS Laravel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL; // Tambahkan ini
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Tambahkan baris ini untuk memaksa semua URL dihasilkan dengan HTTPS
+        if (env('APP_ENV') === 'production' || env('APP_ENV') === 'staging') { // Hanya jalankan di produksi/staging
+             URL::forceScheme('https');
+        }
     }
 }
